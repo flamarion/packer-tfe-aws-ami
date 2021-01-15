d38de738840602e338eaeef428cd4d0b3dd786d8
Flamarion Jorge <flamarion@hashicorp.com>
2021-01-15 18:06:27 +0100

    Adding HA licence to the image.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 mv -v /home/ubuntu/conf/hashicorp-emea-support.rli /etc/license.rli
+mv -v /home/ubuntu/conf/hashicorp-internal-ha-support.rli /etc/license-ha.rli
 mv -v /home/ubuntu/conf/* /etc/
